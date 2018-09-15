fc9110c15dfd450001dcda6e774bf919bcf0942e
Jon Fazzaro <jfazzaro@apterainc.com>
2018-09-15 18:59:42 -0400

    clean: folders

---
+++ pack.cmd
@@ -0,0 +1,4 @@
+nuget pack Scribe\Scribe.NET.nuspec
+nuget pack Scribe.MSTest\Scribe.NET.MSTest.nuspec
+nuget pack Scribe.NUnit\Scribe.NET.NUnit.nuspec
+nuget pack Scribe.xUnit\Scribe.NET.xUnit.nuspec
